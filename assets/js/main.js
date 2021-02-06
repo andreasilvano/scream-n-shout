7255593cf2c5a3daa318e00f3f117820729cf261
Andrea Silvano <andrea.silvano97@gmail.com>
2021-02-06 17:06:05 +0100

    WIP #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log("js is running");
+
+  var vid = document.getElementById('vid1');
+
+function unmute(){
+  if(vid.muted){
+  vid.muted = false;
+} else {
+  vid.muted = true;
+}};
+
+  function play1(){
+    vid.removeAttribute("src");
+    vid.setAttribute("src",'assets/video/muna1.mp4');
+    goback1();
+  };
+
+  function goback1(){
+  vid.removeAttribute("loop");
+  vid.removeAttribute("muted");
+  unmute();
+  vid.onended = function() {
+  vid.removeAttribute("src");
+  vid.setAttribute("src",'assets/video/manfro1.mp4',);
+
+}};
+  
